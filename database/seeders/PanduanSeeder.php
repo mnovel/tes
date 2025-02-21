a4162538df98ea85722f507750d56071ef60aa14
Muhammad Novel <mnovel78@gmail.com>
2025-02-21 21:17:21 +0700

    Fix Panduan Seeder

--- database/seeders/PanduanSeeder.php
+++ database/seeders/PanduanSeeder.php
@@ -14,7 +14,7 @@ class PanduanSeeder extends Seeder
     public function run(): void
     {
         $panduan = [
-            'description' => '<ol>\n  <li>Tes ini bukan sekedar kuis biasa – ini adalah penilaian untuk <strong>memahami kecerdasanmu</strong></li>\n  <li>Tujuan tes ini adalah untuk membantumu dalam menemukan potensi terbaik dalam dirimu</li>\n  <li>Hasil dari tes ini tidak menentukan tingkat kecerdasan, tetapi tipe kecerdasan yang kamu miliki</li>\n  <li>Tidak ada batas waktu, cukup kerjakan secara jujur dengan memilih jawaban yang paling sesuai denganmu</li>\n  <li>Soal akan terdiri dari 2 sesi, perbedaannya hanya dalam pemilihan jawaban. Sesi 1 kamu hanya dapat memilih 1 jawaban diantara “setuju”/”tidak setuju”. Sedangkan sesi 2 kamu dapat memilih 3 jawaban yang paling sesuai dengan dirimu</li>\n</ol>',
+            'description' => '<ol><li>Tes ini bukan sekedar kuis biasa – ini adalah penilaian untuk <strong>memahami kecerdasanmu</strong></li><li>Tujuan tes ini adalah untuk membantumu dalam menemukan potensi terbaik dalam dirimu</li><li>Hasil dari tes ini tidak menentukan tingkat kecerdasan, tetapi tipe kecerdasan yang kamu miliki</li><li>Tidak ada batas waktu, cukup kerjakan secara jujur dengan memilih jawaban yang paling sesuai denganmu</li><li>Soal akan terdiri dari 2 sesi, perbedaannya hanya dalam pemilihan jawaban. Sesi 1 kamu hanya dapat memilih 1 jawaban diantara “setuju”/”tidak setuju”. Sedangkan sesi 2 kamu dapat memilih 3 jawaban yang paling sesuai dengan dirimu</li>\n</ol>',
         ];
         Panduan::create($panduan);
     }
