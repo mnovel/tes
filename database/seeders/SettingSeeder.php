8c4f7a31883713bb305c9439d18fecd44fab3c6f
Muhammad Novel <mnovel78@gmail.com>
2025-02-24 16:40:18 +0700

    Update Setting Seeder

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -15,15 +15,19 @@ class SettingSeeder extends Seeder
     {
         Setting::create([
             'title' => 'Career The Explorer',
-            'description' => 'Voluptate qui ipsum velit occaecat qui fugiat aliqua id nulla eiusmod nisi incididunt reprehenderit.',
+            'description' => 'Career The Explorer adalah platform yang membantu individu menemukan minat dan bakat mereka serta memberikan panduan untuk memilih jurusan kuliah yang tepat.',
             'author' => 'CTE Team',
-            'keywords' => 'tes,minat bakat,kuliah',
+            'keywords' => 'career, explorer, platform, minat, bakat, jurusan kuliah, panduan',
             'icon' => 'images/icon.png',
             'favicon' => 'images/favicon.png',
             'contact' => [
                 'email' => 'helpdesk@careertheexplorer.com',
                 'phone' => '+6287865790888',
                 'address' => 'Jl. Ketintang Madya II no. 42, Surabaya',
+            ],
+            'seo' => [
+                'google_analytics' => '',
+                'bing_webmaster' => ''
             ]
         ]);
     }
